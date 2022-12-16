04df299fe189d9d7f8d498dcd87dfa046334f65f
vaspavliuk <vaspavliuk@gmail.com>
2022-12-16 14:37:17 +0200

    Add project files.

---
+++ List/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            list.Insert(1);
+            list.Insert(4);
+
+            Console.WriteLine(list.Get(1));
+        }
+    }
+
+    public class List<T>
+    {
+        T[] baseArray = new T[2];
+        int arrayEndElement = -1;
+
+        public T Get (int pos)
+        {
+            if (baseArray.Length < pos)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (baseArray[pos] == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return baseArray[pos];
+        }
+
+        public void Insert (T input)
+        {
+            arrayEndElement++;
+
+            if (baseArray.Length > arrayEndElement)
+            {
+                baseArray[arrayEndElement] = input;
+            }
+            //if array is full it shoud be increased an then add a new member to the end
+            else
+            {
+                IncreaseSize(arrayEndElement);
+                baseArray[arrayEndElement] = input;
+            }
+        }
+        
+        // this method should increase the size of array
+        public T[] IncreaseSize (int input)
+        {
+            return new T[input*2];
+        }
+    }
+}
